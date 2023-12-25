0aace53c0e6f8a4ecae55559e9ffb89ca836a6ae
Daniel Febrero <febrero.daniel@gmail.com>
2023-12-25 08:11:18 +0100

    Add support for scss stylesheet

--- apapiai-webapp/src/features/Chat/index.tsx
+++ apapiai-webapp/src/features/Chat/index.tsx
@@ -1,7 +1,16 @@
+import "./style.scss"
+
 const Chat: React.FC = () => {
   return (
     <div>
-      <h1>Chat</h1>
+      <h2>Chat</h2>
+      <div id="chat-container">
+        <div id="chat-messages" />
+        <form id="chat-form">
+          <input id="msg" type="text" placeholder="Enter Message" required />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   )
 }
--- apapiai-webapp/src/pages/Welcome/index.tsx
+++ apapiai-webapp/src/pages/Welcome/index.tsx
@@ -3,17 +3,19 @@ import { useNavigate, useLocation } from "react-router-dom"
 import GoogleSigninButton from "../../features/GoogleSigninButton"
 import Chat from "../../features/Chat"
 
+import "./style.scss"
+
 const Welcome: React.FC = () => {
   const navigate = useNavigate()
   const location = useLocation()
   if (location.pathname !== "/") navigate("/")
 
   return (
-    <>
+    <div id="welcome-container">
       <h1>Welcome to apapiai</h1>
       <GoogleSigninButton />
       <Chat />
-    </>
+    </div>
   )
 }
 
