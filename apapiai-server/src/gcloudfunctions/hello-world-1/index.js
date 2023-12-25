1722f1ff8a37c08ae31bb58f1ec15bd8e33f71cd
Daniel Febrero <febrero.daniel@gmail.com>
2023-12-25 04:36:56 +0100

    Basic webhook for Dialogflow CX

--- apapiai-server/src/gcloudfunctions/hello-world-1/index.js
+++ apapiai-server/src/gcloudfunctions/hello-world-1/index.js
@@ -3,5 +3,16 @@ const functions = require('@google-cloud/functions-framework');
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
 functions.http('helloHttp', (req, res) => {
-  res.send('Hello World!');
+  const response = {
+    fulfillmentResponse: {
+      "messages": [
+        {
+          "text": {
+            "text": ["Hello, world!"]
+          }
+        }
+      ]
+    }
+  }
+  res.json(response);
 });
