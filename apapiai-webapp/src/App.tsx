02c59ab3311cb68a068b076b31d9ebb906a22e7d
Daniel Febrero <febrero.daniel@gmail.com>
2023-12-25 07:42:32 +0100

    Redirect to / and clean App.tsx

--- apapiai-webapp/src/App.tsx
+++ apapiai-webapp/src/App.tsx
@@ -3,35 +3,19 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import Welcome from './pages/Welcome'
 
-export default function App() {
+ const App: React.FC = () => {
   return (
     <GoogleOAuthProvider clientId="705932432287-6ekvke50s3ifkbhvu5tg7mos1gas393n.apps.googleusercontent.com">
       <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
-
           <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/users" element={<Users />} />
             <Route path="/" element={<Welcome />} />
+            <Route path="*" element={<Welcome />} />
           </Routes>
         </div>
       </Router>
@@ -39,14 +23,4 @@ export default function App() {
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+export default App;
--- apapiai-webapp/src/pages/Welcome.tsx
+++ apapiai-webapp/src/pages/Welcome.tsx
@@ -1,6 +1,11 @@
+import { useNavigate, useLocation } from "react-router-dom";
+
 import GoogleSigninButton from "../features/GoogleSigninButton"
 
 const Welcome: React.FC = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    if (location.pathname !== "/") navigate("/");
     return <GoogleSigninButton />
 }
 
