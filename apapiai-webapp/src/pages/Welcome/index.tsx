70f2bd417fc4036e05bd96c769e3eb7531a7f56e
Daniel Febrero <febrero.daniel@gmail.com>
2023-12-25 10:00:20 +0100

    Hide login button once logged in

--- apapiai-webapp/src/pages/Welcome/index.tsx
+++ apapiai-webapp/src/pages/Welcome/index.tsx
@@ -1,13 +1,18 @@
 import GoogleSigninButton from "../../features/GoogleSigninButton"
 import Chat from "../../features/Chat"
 
+import useRedux from "../../hooks/useRedux"
+
 import "./style.scss"
 
 const Welcome: React.FC = () => {
+  const { useAppSelector } = useRedux
+  const user = useAppSelector((state) => state.user)
+
   return (
     <div id="welcome-container">
       <h1>Welcome to apapiai</h1>
-      <GoogleSigninButton />
+      {user.credential === undefined && <GoogleSigninButton />}
       <Chat />
     </div>
   )
