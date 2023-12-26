f9e39c09f7abed5e845e3b3fe45e9248dbb10914
Daniel Febrero <febrero.daniel@gmail.com>
2023-12-26 12:45:57 +0100

    Type flow from dialogflow cx

--- apapiai-server/src/controllers/dialogflow.ts
+++ apapiai-server/src/controllers/dialogflow.ts
@@ -4,27 +4,29 @@ import {
   SessionsClient,
 } from "@google-cloud/dialogflow-cx/build/src/v3beta1"
 
-export type AgentClientType = (typeof AgentsClient)["prototype"]["getAgent"]
+type AgentsClientType = (typeof AgentsClient)["prototype"]["getAgent"]
+type FlowsClientType = (typeof AgentsClient)["prototype"]["getAgent"]
 type Callback<T, U, V> = (arg1: T, arg2: U, arg3: V) => void
 type ExtractIAgent<T> = T extends Callback<any, infer Test, any> ? Test : never
-type FirstOverload = Awaited<
-  AgentClientType extends {
+type FirstOverloadOfAgentsClient = Awaited<
+  AgentsClientType extends {
+    (arg1: infer P1, arg2: infer P2): infer R
+  }
+    ? [P1, P2, R]
+    : never
+>[1]
+type FirstOverloadOfFlowClient = Awaited<
+  FlowsClientType extends {
     (arg1: infer P1, arg2: infer P2): infer R
   }
     ? [P1, P2, R]
     : never
 >[1]
 
-type AgentType = NonNullable<ExtractIAgent<FirstOverload>>
+type AgentType = NonNullable<ExtractIAgent<FirstOverloadOfAgentsClient>>
+type FlowType = NonNullable<ExtractIAgent<FirstOverloadOfFlowClient>>
 
-export const startConversation = async (agent: AgentType) => {
-  // agent.
-}
-
-export const addMessageToConversation = async (
-  conversationId: string,
-  message: string
-): Promise<void> => {
+export const startConversation = async () => {
   const agentClient = new AgentsClient({
     apiEndpoint: "us-central1-dialogflow.googleapis.com",
   })
@@ -36,9 +38,14 @@ export const addMessageToConversation = async (
   const flowClient = new FlowsClient({
     apiEndpoint: "us-central1-dialogflow.googleapis.com",
   })
-  const flow = await flowClient.getFlow({
+  const flow = (await flowClient.getFlow({
     name: agent?.startFlow,
-  })
+  })) as unknown as FlowType[]
 
   console.log({ agent, flow })
 }
+
+export const addMessageToConversation = async (
+  conversationId: string,
+  message: string
+): Promise<void> => {}
