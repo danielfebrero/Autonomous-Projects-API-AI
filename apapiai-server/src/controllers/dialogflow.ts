bdb1d07649eb7663d6dc57a8ce1e141d6ca41a16
Daniel Febrero <febrero.daniel@gmail.com>
2023-12-26 05:48:47 +0100

    Get Agent from dialogflow cx

--- apapiai-server/src/controllers/dialogflow.ts
+++ apapiai-server/src/controllers/dialogflow.ts
@@ -1,21 +1,17 @@
-import { SessionsClient } from "@google-cloud/dialogflow-cx/build/src/v3beta1"
+import {
+  SessionsClient,
+  AgentsClient,
+} from "@google-cloud/dialogflow-cx/build/src/v3beta1"
 
 export const addMessageToConversation = async (
   conversationId: string,
   message: string
 ): Promise<void> => {
-  const request = {
-    queryInput: {
-      text: {
-        text: message,
-        languageCode: "fr-FR",
-      },
-    },
-  }
-  const sessionClient = new SessionsClient({ projectId: "bard-407521" })
-  const intent = await sessionClient.matchIntent(request)
-  const fulfilledIntent = await sessionClient.fulfillIntent({
-    matchIntentRequest: intent,
-  } as any)
-  console.log({ sessionClient, intent, fulfilledIntent })
+  const agentClient = new AgentsClient({
+    apiEndpoint: "us-central1-dialogflow.googleapis.com",
+  })
+  const agent = await agentClient.getAgent({
+    name: `projects/${process.env.PROJECT_ID}/locations/us-central1/agents/${process.env.AGENT_ID}`,
+  })
+  console.log({ agent })
 }
