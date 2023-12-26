04a5cf743e50a7abdbb1fb3e3eb570c23cd6906e
Daniel Febrero <febrero.daniel@gmail.com>
2023-12-26 06:25:30 +0100

    Extract IAgent from other types

--- apapiai-server/src/controllers/dialogflow.ts
+++ apapiai-server/src/controllers/dialogflow.ts
@@ -1,7 +1,19 @@
-import {
-  SessionsClient,
-  AgentsClient,
-} from "@google-cloud/dialogflow-cx/build/src/v3beta1"
+import { AgentsClient } from "@google-cloud/dialogflow-cx/build/src/v3beta1"
+
+export type AgentClientType = (typeof AgentsClient)["prototype"]["getAgent"]
+type Callback<T, U, V> = (arg1: T, arg2: U, arg3: V) => void
+type ExtractIAgent<T> = T extends Callback<any, infer Test, any> ? Test : never
+type FirstOverload = Awaited<
+  AgentClientType extends {
+    (arg1: infer P1, arg2: infer P2): infer R
+  }
+    ? [P1, P2, R]
+    : never
+>[1]
+
+type AgentType = NonNullable<ExtractIAgent<FirstOverload>>
+
+export const startConversation = async (agent: AgentType) => {}
 
 export const addMessageToConversation = async (
   conversationId: string,
@@ -13,5 +25,6 @@ export const addMessageToConversation = async (
   const agent = await agentClient.getAgent({
     name: `projects/${process.env.PROJECT_ID}/locations/us-central1/agents/${process.env.AGENT_ID}`,
   })
+
   console.log({ agent })
 }
