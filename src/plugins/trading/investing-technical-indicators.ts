e5172c10a07817c8ca40807cb0b09ce2c89c175b
Daniel Febrero <febrero.daniel@gmail.com>
2023-12-30 16:27:38 +0100

    parallelize investing plugin promises

--- src/plugins/trading/investing-technical-indicators.ts
+++ src/plugins/trading/investing-technical-indicators.ts
@@ -5,39 +5,47 @@ import { goto } from "../../controllers/browse"
 export const getTechnicalIndicatorsFromInvestingAndMarketData = async (
   symbol: string
 ) => {
-  const { page, browser } = await goto(
-    `https://www.investing.com/currencies/${symbol}-technical`
-  )
+  const technicalIndicatorsFromInvesting = async () => {
+    return new Promise(async (resolve, reject) => {
+      const { page, browser } = await goto(
+        `https://www.investing.com/currencies/${symbol}-technical`
+      )
 
-  try {
-    await page.waitForSelector("#onetrust-accept-btn-handler", {
-      timeout: 5000,
+      try {
+        await page.waitForSelector("#onetrust-accept-btn-handler", {
+          timeout: 5000,
+        })
+        await page.click("#onetrust-accept-btn-handler")
+      } catch (error) {
+        console.log(error)
+      }
+
+      var responseFromGPT, selectorContent
+
+      try {
+        const selector = await page.waitForSelector("#techinalContent")
+        const img = await selector?.screenshot()
+        const base64 = img?.toString("base64") ?? ""
+        selectorContent = await selector?.evaluate((el) => el.textContent)
+
+        await browser.close()
+
+        responseFromGPT = await vision({
+          base64,
+          instruction:
+            "Identify and list the financial trading indicators including moving averages, technical indicators, and pivot points from the image provided. Return as a structured JSON object.",
+        })
+        resolve(responseFromGPT)
+      } catch (error) {
+        reject(error)
+      }
     })
-    await page.click("#onetrust-accept-btn-handler")
-  } catch (error) {
-    console.log(error)
   }
 
-  var responseFromGPT, selectorContent
-
-  try {
-    const selector = await page.waitForSelector("#techinalContent")
-    const img = await selector?.screenshot()
-    const base64 = img?.toString("base64") ?? ""
-    selectorContent = await selector?.evaluate((el) => el.textContent)
-
-    await browser.close()
-
-    responseFromGPT = await vision({
-      base64,
-      instruction:
-        "Identify and list the financial trading indicators including moving averages, technical indicators, and pivot points from the image provided. Return as a structured JSON object.",
-    })
-  } catch (error) {
-    console.log(error)
-  }
-
-  const quotes = await getQuote({ symbol })
+  const [quotes, responseFromGPT] = await Promise.all([
+    getQuote({ symbol }),
+    technicalIndicatorsFromInvesting(),
+  ])
 
   return {
     responseFromGPT,
