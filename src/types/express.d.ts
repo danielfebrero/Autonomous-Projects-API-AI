5e2a3a26a077f84b789a5c91303312ff1da80bda
Daniel Febrero <febrero.daniel@gmail.com>
2024-01-07 18:02:59 +0100

    remove typescript eslint stuff

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,5 @@
 import { Request } from "express"
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-
 export type RequestCake = Request & {
   calculatedData: {
     [key: string]: any
